7950a44bcfefc2722ce995cfd4dd5a042019b702
assassin_box <assassin543@outlook.jp>
2020-07-04 22:32:39 +0900

    小窓追加

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 
 
 const card01 = <HTMLImageElement>document.getElementById('card01')
+const test = <HTMLButtonElement>document.getElementById('test-b')
 
 card01?.addEventListener("click", () => {
-      alert(`unkotinko`)
+      // alert(`unkotinko`)
+})
 
-
-
+test?.addEventListener("click", () => {
+      alert(`tinko`)
 })
